0074fd35b253a4aeb4742decd5c831883ef8b649
stacksagar <mr.sagar.co@gmail.com>
2021-09-18 21:06:02 +0600

    updated

--- src/components/documentation/DocNavBar.tsx
+++ src/components/documentation/DocNavBar.tsx
@@ -15,7 +15,7 @@ export default function DocNavBar({
   };
 
   return (
-    <div className="w-full block screen_900:hidden h-header_height border-b border-gray-500 border-opacity-30">
+    <div className="w-full hidden screen_500:block screen_900:hidden h-header_height border-b border-gray-500 border-opacity-30">
       <div className="responsive h-full flex items-center space-x-3">
         {titles.map((title, i) => (
           <p
